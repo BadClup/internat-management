a0f9a3e3fe01571cc4aee02498cecc0849d35c9f
Bartłomiej Strama <kontakt@bstrama.com>
2024-03-27 00:00:42 +0100

    improvement: quick schema fix

--- server/db_docker/schema.sql
+++ server/db_docker/schema.sql
@@ -1,5 +1,11 @@
 CREATE TYPE user_role AS ENUM ('supervisor', 'resident');
 
+CREATE TYPE lat_long AS
+(
+    latitude  NUMERIC,
+    longitude NUMERIC
+);
+
 CREATE TABLE "user"
 (
     id          SERIAL PRIMARY KEY,
@@ -7,7 +13,7 @@ CREATE TABLE "user"
     password    VARCHAR(255) NOT NULL, -- checking password length here is pointless because it is hashed
     first_name  VARCHAR(255) NOT NULL,
     last_name   VARCHAR(255) NOT NULL,
-    room_number int          NOT NULL CHECK ( room_number > 0 ),
+    room_number int CHECK ((role = 'resident' AND room_number > 0) OR role != 'resident'),
     role        user_role    NOT NULL
 );
 
@@ -30,10 +36,13 @@ CREATE TABLE "exit_request_message"
 (
     id                    SERIAL PRIMARY KEY,
     message_id            INT REFERENCES "message" (id) ON DELETE CASCADE,
-    current_user_location VARCHAR(255) NOT NULL,
-    desired_location      VARCHAR(255) NOT NULL,
+    initial_location      lat_long     NOT NULL,
+    desired_location_name VARCHAR(255) NOT NULL,
     request_content       TEXT,
-    approved              BOOLEAN      NOT NULL                                  DEFAULT FALSE,
-    approved_by           INT          REFERENCES "user" (id) ON DELETE SET NULL DEFAULT NULL,
-    approved_at           TIMESTAMP                                              DEFAULT NULL
+
+    approved_by           INT          REFERENCES "user" (id) ON DELETE SET NULL,
+    approved_at           TIMESTAMP,
+
+    came_back_at          TIMESTAMP,
+    came_back_approved_by INT          REFERENCES "user" (id) ON DELETE SET NULL,
 );
