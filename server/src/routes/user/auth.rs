3e9d5929c4d1e956e87bac36fe928b324e820c3b
Bartłomiej Strama <kontakt@bstrama.com>
2024-04-02 13:50:44 +0200

    Tests extended and fixed

--- server/src/routes/user/auth.rs
+++ server/src/routes/user/auth.rs
@@ -1,9 +1,11 @@
+use std::str::FromStr;
 use axum::{Extension, Json};
 use axum::http::{HeaderMap, StatusCode};
 use axum::response::{IntoResponse, Response};
 use axum_extra::TypedHeader;
+use axum_test::http::HeaderName;
 use axum_test::TestServer;
-use headers::Authorization;
+use headers::{Authorization, HeaderValue};
 use headers::authorization::Bearer;
 use hmac::digest::KeyInit;
 use hmac::Hmac;
@@ -240,35 +242,6 @@ pub async fn register_residents<'a>(
     return ApiResult::Ok(Json(users_credentials));
 }
 
-#[tokio::test]
-async fn test_users_register() {
-    let users_data = vec![
-        UserRegisterDto {
-            username: "test1".to_string(),
-            first_name: "Test".to_string(),
-            last_name: "One".to_string(),
-            room_nr: 1,
-        },
-        UserRegisterDto {
-            username: "test2".to_string(),
-            first_name: "Test".to_string(),
-            last_name: "Two".to_string(),
-            room_nr: 2,
-        },
-    ];
-
-    let app_state = AppState::new().await;
-    let app = crate::get_app(app_state.clone());
-    let server = TestServer::new(app).expect("Failed to create test server");
-
-    let res = server.post("/user/register-many")
-        .content_type("application/json")
-        .json(&json!(users_data))
-        .await;
-
-    res.assert_status_unauthorized();
-}
-
 fn sha512(data: &str) -> String {
     let mut hasher = Sha512::new();
     hasher.update(data.as_bytes());
@@ -295,3 +268,68 @@ fn test_generate_random_password() {
     let password = generate_random_password(2137);
     assert_eq!(password.len(), 2137);
 }
+
+#[tokio::test]
+async fn test_users_register() {
+    let users_data = vec![
+        UserRegisterDto {
+            username: "test1".to_string(),
+            first_name: "Test".to_string(),
+            last_name: "One".to_string(),
+            room_nr: 1,
+        },
+        UserRegisterDto {
+            username: "test2".to_string(),
+            first_name: "Test".to_string(),
+            last_name: "Two".to_string(),
+            room_nr: 2,
+        },
+    ];
+
+    let app_state = AppState::new().await;
+    let app = crate::get_app(app_state.clone());
+    let server = TestServer::new(app).expect("Failed to create test server");
+
+    struct TestUser {
+        user_role: UserRole,
+        bearer_token: String,
+    }
+
+    let test_users = vec![
+        // "bstrama" account from seed.sql
+        TestUser {
+            user_role: UserRole::Resident,
+            bearer_token: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJic3RyYW1hIiwiZmlyc3RfbmFtZSI6IkJhcnTFgm9taWVqIiwibGFzdF9uYW1lIjoiU3RyYW1hIiwicm9vbV9uciI6MSwicm9sZSI6IlJlc2lkZW50In0.NDRhw0XKORAiAeCP2uS0Z2-2wema8A_9nHNiScZkJyA".to_owned(),
+        },
+        // "kierowniczka" account from seed.sql
+        TestUser {
+            user_role: UserRole::Supervisor,
+            bearer_token: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwidXNlcm5hbWUiOiJraWVyb3duaWN6a2EiLCJmaXJzdF9uYW1lIjoiQm9ndW1pxYJhIiwibGFzdF9uYW1lIjoiS2FwdHVya2lld2ljeiIsInJvb21fbnIiOm51bGwsInJvbGUiOiJTdXBlcnZpc29yIn0.SlkzM77qydnK3raBIfwfVtisdwXy5QzgvRoUVEspsMI".to_owned(),
+        },
+    ];
+
+    for user in test_users {
+        let res = server.post("/user/register-many")
+            .content_type("application/json")
+            .add_header(
+                HeaderName::from_str("Authorization").unwrap(),
+                HeaderValue::from_str(&user.bearer_token).unwrap(),
+            )
+            .json(&json!(users_data))
+            .await;
+
+        match user.user_role {
+            UserRole::Resident => res.assert_status_forbidden(),
+            UserRole::Supervisor => {
+                res.assert_status_ok();
+
+                // cleanup
+                sqlx::query!("DELETE FROM \"user\" WHERE username = 'test1' OR username = 'test2'")
+                    .execute(&app_state.db_pool)
+                    .await
+                    .expect("Failed to delete test users");
+            }
+        }
+    }
+}
+
