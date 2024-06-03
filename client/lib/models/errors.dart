99868c95a61858cca36310b4d2d320131bf933bf
Bartłomiej Strama <80683535+BaderBC@users.noreply.github.com>
2024-06-03 11:51:04 +0200

    Revert "Flutter setup"

--- client/lib/models/errors.dart
+++
@@ -1,8 +0,0 @@
-class UserErrors {
-
-  final String? loginInputError;
-  final String? passwordInputError;
-  final String? mainError;
-
-  UserErrors({this.loginInputError, this.passwordInputError, this.mainError});
-}
--- client/lib/models/navigation_option.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class NavOption {
-  String route;
-  String label;
-  Icon icon;
-
-  NavOption({required this.route, required this.label, required this.icon});
-}
