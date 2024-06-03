ab85334c680107b2c4873561397e6a90a6733f76
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2024-06-03 11:46:48 +0200

    feat: add chat bloc
    - add chat bloc with chat_state and chat_events
    - add chat model with functions to get and send messages
    - add event for getting messages while entering a chat screen
    - small changes in theme colors (still unsure about ui)
    - add id property in user class

---
+++ client/lib/screens/chat/sendMessageBox.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../blocs/chat/chat_bloc.dart';
+import '../../blocs/theme/theme_bloc.dart';
+import '../../blocs/user/user_bloc.dart';
+import '../../models/theme.dart';
+
+class SendMessagebox extends StatelessWidget {
+  SendMessagebox({super.key});
+
+  final _messageTextFieldController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<UserBloc, UserState>(
+      builder: (context, state) {
+        return Container(
+          height: 70,
+          color:
+              AppTheme.darkTheme == context.watch<ThemeBloc>().state.themeData
+                  ? AppColors.primaryAccent
+                  : Colors.deepPurple[200],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                    child: TextField(controller: _messageTextFieldController)),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: IconButton.filled(
+                      onPressed: () {
+                        final content = _messageTextFieldController.text.trim();
+
+                        context.read<ChatBloc>().add(SendMessage(
+                            content: content,
+                            bearerToken: state.bearerToken!,
+                            residentId: state.user.id!));
+                      },
+                      icon: const Icon(Icons.send)),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
